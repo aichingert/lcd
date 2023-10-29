0d881115b56e93180c2f2d6fa8ed2ba939e78e4a
aichingert <aichinger.tobias13@gmail.com>
2023-10-29 11:58:54 +0100

    feat: solution [2023-10-29]

--- src/solution/hard/mod.rs
+++ src/solution/hard/mod.rs
@@ -1,8 +1,19 @@
 pub mod count_vowels_permutation;
+pub mod poor_pigs;
 
 #[cfg(test)]
 mod test {
     use crate::Solution;
+
+    #[test]
+    fn nr_0458_poor_pigs_ex_01() {
+        assert_eq!(2, Solution::poor_pigs(4, 15, 15));
+    }
+
+    #[test]
+    fn nr_0458_poor_pigs_ex_02() {
+        assert_eq!(2, Solution::poor_pigs(4, 15, 30));
+    }
 
     #[test]
     fn nr_1220_count_vowel_permutation_ex_01() {
