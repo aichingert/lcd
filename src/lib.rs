7ba9279606249f127d8d73b47cafa3fa3f4b2c47
aichingert <aichinger.tobias13@gmail.com>
2023-11-01 10:35:13 +0100

    feat: solution [2023-11-01] easy

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,27 @@
  * be used in more than one day
  */
 
+use std::rc::Rc;
+use std::cell::RefCell;
 
 pub mod solution;
 pub use solution::Solution;
+
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    pub fn new(val: i32) -> Self {
+        Self {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
 
 pub fn is_palindrome(s: &[char]) -> bool  {
     for i in 0..s.len() / 2 {
--- src/solution/easy/mod.rs
+++ src/solution/easy/mod.rs
@@ -1,8 +1,34 @@
 pub mod sort_integers_by_the_number_of_1_bits;
+pub mod find_mode_in_binary_search_tree;
 
 #[cfg(test)]
 mod test {
+    use std::rc::Rc;
+    use std::cell::RefCell;
+
     use crate::Solution;
+    use crate::TreeNode;
+
+    #[test]
+    fn nr_0501_find_mode_ex_01() {
+        let root = Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: None,
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode::new(2)))),
+                right: None,
+            })))
+        })));
+
+        assert_eq!(vec![2], Solution::find_mode(root));
+    }
+
+    #[test]
+    fn nr_0501_find_mode_ex_02() {
+        let root = Some(Rc::new(RefCell::new(TreeNode::new(0))));
+        assert_eq!(vec![0], Solution::find_mode(root));
+    }
 
     #[test]
     fn nr_1356_sort_by_bits_ex_01() {
