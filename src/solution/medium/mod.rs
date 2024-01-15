f63dd18e08847d05fa05af7274a9174ab2372721
aichingert <aichinger.tobias13@gmail.com>
2024-01-15 20:35:39 +0100

    feat: sol 2024-01-15 med

--- src/solution/medium/mod.rs
+++ src/solution/medium/mod.rs
@@ -20,6 +20,7 @@ pub mod m2870;
 pub mod l300;
 pub mod m1347;
 pub mod d1657;
+pub mod f2225;
 
 #[cfg(test)]
 mod test {
@@ -28,6 +29,16 @@ mod test {
 
     use crate::TreeNode;
     use crate::Solution;
+
+    #[test]
+    fn nr_2225_ex_01() {
+        assert_eq!(vec![vec![1,2,10], vec![4,5,7,8]], Solution::find_winners(vec![vec![1,3],vec![2,3],vec![3,6],vec![5,6],vec![5,7],vec![4,5],vec![4,8],vec![4,9],vec![10,4],vec![10,9]]));
+    }
+
+    #[test]
+    fn nr_2225_ex_02() {
+        assert_eq!(vec![vec![1,2,5,6], vec![]], Solution::find_winners(vec![vec![2,3],vec![1,3],vec![5,4],vec![6,4]]));
+    }
 
     #[test]
     fn nr_1657_ex_01() {
